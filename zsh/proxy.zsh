326149ec771448cce6426138c4ae4bc46d92c28e
wuqin <1392632107@qq.com>
2025-06-20 17:23:55 +0800

    优化 proxy 代理

--- zsh/proxy.zsh
+++ zsh/proxy.zsh
@@ -1,7 +1,16 @@
 proxy() {
-    hostip=$(ip route | grep default | awk '{print $3}')
+    # 设置代理，参数 -h 指定hostip，-s 指定socks_hostport，-p 指定http_hostport
+    # hostip=$(ip route | grep default | awk '{print $3}')
+    hostip="localhost"
     socks_hostport=7890
     http_hostport=7890
+    while getopts "h:s:p:" opt; do
+        case $opt in
+            h) hostip=$OPTARG ;;
+            s) socks_hostport=$OPTARG ;;
+            p) http_hostport=$OPTARG ;;
+        esac
+    done
     export https_proxy="http://${hostip}:${http_hostport}"
     export http_proxy="http://${hostip}:${http_hostport}"
     export ALL_PROXY="socks5://${hostip}:${socks_hostport}"
@@ -15,6 +24,10 @@ unproxy() {
     unset https_proxy
     unset http_proxy
     unset all_proxy
+}
+
+testproxy() {
+    curl -v google.com
 }
 
 listproxy() {
