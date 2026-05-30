06c00933b2936cdcf145efb893e6943bd18b743d
wuqin <1392632107@qq.com>
2026-05-30 15:03:25 +0800

    增加指定matplotlib,jupyter,less的数据文件位置

--- zsh/envs.zsh
+++ zsh/envs.zsh
@@ -6,8 +6,6 @@ export XDG_STATE_HOME=$HOME/.local/state # 程序状态文件
 
 # 默认程序
 export EDITOR=nvim  # 默认编辑器
-export VISUAL=nvim  # 默认编辑器
-export BROWSER=wyeb # 默认浏览器
 
 # conda
 export TERMINFO=/usr/share/terminfo # 解决 conda环境下 clear 报错
@@ -37,16 +35,30 @@ export GRADLE_USER_HOME=${XDG_DATA_HOME}/gradle
 export NPM_CONFIG_USERCONFIG=${XDG_CONFIG_HOME}/npm/npm.conf
 
 # cuda
-export CUDA_CACHE_PATH=${XDG_CACHE_HOME}/nv
-export CUDA_PATH=/usr/local/cuda
-export CUDA_HOME=/usr/local/cuda
-export CUDNN_PATH=/usr/local/cuda/include
+if command -v nvidia-smi >/dev/null 2>&1; then
+    export CUDA_CACHE_PATH=${XDG_CACHE_HOME}/nv
+    export CUDA_PATH=/usr/local/cuda
+    export CUDA_HOME=/usr/local/cuda
+    export CUDNN_PATH=/usr/local/cuda/include
+    export PATH="/usr/local/cuda/bin:$PATH"
+    export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
+fi
 
 # python
 export PYTHONPYCACHEPREFIX=$XDG_CACHE_HOME/python
 export PYTHONSTARTUP=$XDG_CONFIG_HOME/python/startup.py # python 启动时运行的文件
 export PYTHONHISTFILE=$XDG_STATE_HOME/python/history # python 历史命令记录
 export IPYTHONDIR=$XDG_STATE_HOME/python/ipython # ipython 目录
+export MPLCONFIGDIR="$XDG_CONFIG_HOME/matplotlib" # matplotlib 配置目录
+
+# jupyter
+export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
+export JUPYTER_DATA_DIR="$XDG_DATA_HOME/jupyter"
+export JUPYTER_RUNTIME_DIR="${XDG_RUNTIME_DIR:-$TMPDIR}/jupyter"
+
+# less
+export LESSHISTFILE="$XDG_STATE_HOME/less/history"
+export LESSKEYIN="$XDG_CONFIG_HOME/lesskey"
 
 # tensorflow
 export KERAS_HOME=${XDG_CACHE_HOME}/keras
@@ -61,15 +73,7 @@ export WGETRC=${XDG_CONFIG_HOME}/wget/wgetrc
 # export QT_DEBUG_PLUGINS=1 # qt debug模式，显示更多报错信息
 
 # PATH
-_local_bin="$HOME/.local/bin"
-_cuda_bin="/usr/local/cuda/bin"
-export PATH="$GOBIN:$_local_bin:$_cuda_bin:$PATH"
-unset _local_bin _cuda_bin
-
-# LD_LIBRARY_PATH
-_cuda_lib="/usr/local/cuda/lib64"
-export LD_LIBRARY_PATH=$_cuda_lib:${LD_LIBRARY_PATH}
-unset _cuda_lib
+export PATH="$GOBIN:$HOME/.local/bin:$PATH"
 
 # huggingface
 export HF_ENDPOINT=https://hf-mirror.com # huggingface 镜像
@@ -81,17 +85,5 @@ if [[ "$(uname)" == "Darwin" ]]; then
     export HOMEBREW_API_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles/api  #ckbrew
     export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles #ckbrew
     eval $(/opt/homebrew/bin/brew shellenv) #ckbrew
-
-    # miniconda
-    __conda_setup="$('/opt/homebrew/Caskroom/miniconda/base/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-    if [ $? -eq 0 ]; then
-        eval "$__conda_setup"
-    else
-        if [ -f "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
-            . "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh"
-        else
-            export PATH="/opt/homebrew/Caskroom/miniconda/base/bin:$PATH"
-        fi
-    fi
-    unset __conda_setup
+    # export HOMEBREW_NO_INSTALL_FROM_API=1 # 走本地homebrew/cask
 fi
